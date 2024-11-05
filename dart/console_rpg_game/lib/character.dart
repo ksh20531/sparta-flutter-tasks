27dd0ccc049481efa661374e8b929f072e56a8a5
김서후 <ksh20531@gmail.com>
2024-11-05 22:00:07 +0900

    refactor : charactor 클래스 생성자 변경

--- dart/console_rpg_game/lib/character.dart
+++ dart/console_rpg_game/lib/character.dart
@@ -4,18 +4,14 @@ class Character {
   int atk = 0; // 공격력
   int def = 0; // 방어력
 
-  /// 공격
-  void attackMonster() {
+  Character(this.name, this.hp, this.atk, this.def);
 
-  }
+  /// 공격
+  void attackMonster() {}
 
   /// 방어
-  void defend() {
-
-  }
+  void defend() {}
 
   /// 캐릭터 상태 출력
-  void showStatus() {
-
-  }
+  void showStatus() {}
 }

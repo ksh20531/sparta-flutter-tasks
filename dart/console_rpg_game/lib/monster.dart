ae71164d6b59f4ab24a11086db69d802109a7bc8
김서후 <ksh20531@gmail.com>
2024-11-05 20:50:01 +0900

    feat : monster 클래스 정의

---
+++ dart/console_rpg_game/lib/monster.dart
@@ -0,0 +1,16 @@
+class monster {
+  String name = ''; // 몬스터 이름
+  int hp = 0; // 몬스터 체력
+  int maxAtk = 0; // 몬스터 최대 공격력, 방어력보다 낮을 수 없고 랜덤
+  int def = 0; // 몬스터 방어력
+
+  /// 캐릭터 공격
+  void attackCharacter() {
+
+  }
+
+  /// 몬스터 상태 출력
+  void showStatus() {
+
+  }
+}

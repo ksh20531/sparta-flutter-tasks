23ca3244c9ed75660c1a1dec6028ffedd0807834
김서후 <ksh20531@gmail.com>
2024-11-05 22:30:50 +0900

    feat : 캐릭터 이름 입력 받기

--- dart/console_rpg_game/bin/console_rpg_game.dart
+++ dart/console_rpg_game/bin/console_rpg_game.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
-
 import 'package:console_rpg_game/character.dart';
 
 void main(List<String> arguments) {
+  // 캐릭터 파일 정보 읽어오기
   try {
     final characterFile = File('data/csv/character.txt');
     final contents = characterFile.readAsStringSync();
@@ -18,10 +18,21 @@ void main(List<String> arguments) {
     if (characterHp == null) throw 'Hp 초기화 실패';
     if (characterAtk == null) throw 'Atk 초기화 실패';
     if (characterDef == null) throw 'Def 초기화 실패';
-
-    Character character =
-        Character('이름', characterHp, characterAtk, characterDef);
   } catch (e) {
     print('캐릭터 생성에 실패했습니다 : $e');
+  }
+
+  // 캐릭터 이름 입력 받기
+  try {
+    stdout.write("캐릭터의 이름을 입력하세요: ");
+    String characterName = stdin.readLineSync()!;
+
+    // 한글 및 영어 대소문자 정규식
+    RegExp regex = RegExp(r'^[a-zA-Z가-힣]+$');
+
+    if (characterName.isEmpty) throw '이름은 필수에요!';
+    if (!regex.hasMatch(characterName)) throw '한글과 영어만 입력 가능해요';
+  } catch (e) {
+    print(e);
   }
 }

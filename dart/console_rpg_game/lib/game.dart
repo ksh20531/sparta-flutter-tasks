4b2de4e70aefa07987f0964381ac3e0a953ae7bb
김서후 <ksh20531@gmail.com>
2024-11-05 21:02:02 +0900

    feat : game 클래스 정의

---
+++ dart/console_rpg_game/lib/game.dart
@@ -0,0 +1,20 @@
+import 'package:console_rpg_game/monster.dart';
+
+import 'character.dart';
+
+class Game {
+  Character character = Character();
+  List<Monster> monster = []; // 몬스터 리스트
+  int monsterKillCount = 0; // 죽인 몬스터 수
+
+  /// 게임 시작
+  bool startGame() {
+    return true;
+  }
+
+  /// 전투 진행
+  void battle() {}
+
+  /// 랜덤 몬스터 소환
+  void getRandomMonster() {}
+}

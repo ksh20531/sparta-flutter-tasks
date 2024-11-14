4a3c79e3af0f03a0466571016a66b92709fad4de
김서후 <ksh20531@gmail.com>
2024-11-14 20:49:24 +0900

    feat : Station Page UI 구현

---
+++ flutter/flutter_train_app/lib/pages/station/station_list_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_train_app/pages/station/station_name.dart';
+
+class StationListPage extends StatelessWidget {
+  List<String> stationList = [
+    '수서',
+    '동탄',
+    '평택지제',
+    '천안아산',
+    '오송',
+    '대전',
+    '김천구미',
+    '동대구',
+    '경주',
+    '울산',
+    '부산'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('출발역'),
+      ),
+      body: ListView.builder(
+        itemCount: stationList.length,
+        itemBuilder: (BuildContext context, int idx) {
+          return StationName('${stationList[idx]}');
+        },
+      ),
+    );
+  }
+}
---
+++ flutter/flutter_train_app/lib/pages/station/station_name.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class StationName extends StatelessWidget {
+  String stationName;
+  StationName(this.stationName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: 50,
+          child: Text(
+            stationName,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.grey[300]!,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}

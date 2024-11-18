7c31d962a168bc5fb98f678e46d06676d1640fe9
김서후 <ksh20531@gmail.com>
2024-11-19 01:58:25 +0900

    feat : 좌석 선택 로직 추가 및 위젯 변경

--- flutter/flutter_train_app/lib/pages/seat/seat_page.dart
+++ flutter/flutter_train_app/lib/pages/seat/seat_page.dart
@@ -1,13 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const seatNum = 20;
+const rowCnt = 20;
+const List<String> seatHeader = ['A', 'B', ' ', 'C', 'D'];
 
 class SeatPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SeatPageStatus();
 }
 
-class _SeatPageStatus extends State<StatefulWidget> {
+class _SeatPageStatus extends State<SeatPage> {
+  int? selectedSeat;
+  int? selectedRow;
+  String? selectedCol;
+
   @override
   Widget build(BuildContext context) {
     final arguments =
@@ -16,99 +22,144 @@ class _SeatPageStatus extends State<StatefulWidget> {
     String? endStation = arguments[1];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('좌석 선택'),
-      ),
+      appBar: AppBar(title: Text('좌석 선택')),
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    startStation,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.purple,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      startStation,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple,
+                      ),
                     ),
-                  ),
-                  Icon(
-                    Icons.arrow_circle_right_outlined,
-                    size: 30,
-                  ),
-                  Text(
-                    endStation,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.purple,
+                    Icon(
+                      Icons.arrow_circle_right_outlined,
+                      size: 30,
                     ),
-                  ),
-                ],
+                    Text(
+                      endStation,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 5,
-              ),
+              const SizedBox(height: 8),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: 24,
-                    height: 24,
+                    width: 20,
+                    height: 20,
                     decoration: BoxDecoration(
                       color: Colors.purple,
                       borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    child: Text('선택됨'),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
+                  Text('선택됨'),
+                  SizedBox(width: 30),
                   Container(
-                    width: 24,
-                    height: 24,
+                    width: 20,
+                    height: 20,
                     decoration: BoxDecoration(
                       color: Colors.grey[300]!,
                       borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5),
-                    child: Text('선택안됨'),
-                  ),
+                  Text('선택안됨'),
                 ],
               ),
-              RowsHeader(),
-              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(seatHeader.length, (i) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 4),
+                    alignment: Alignment.bottomCenter,
+                    width: 50,
+                    height: 50,
+                    child: Text(seatHeader[i]),
+                  );
+                }),
+              ),
+              const SizedBox(height: 8),
               Expanded(
-                child: ListView(
-                  children: List.generate(seatNum, (i) => Rows(i + 1)),
-                ),
+                child: GridView.builder(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 5,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                    ),
+                    itemCount: rowCnt * seatHeader.length,
+                    itemBuilder: (context, idx) {
+                      String seatChar = seatHeader[idx % 5];
+                      return selectSeat(idx, seatChar);
+                    }),
               ),
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
+                  onPressed: () {
+                    if (selectedRow != null && selectedCol != null) {
+                      showCupertinoDialog(
+                        context: context,
+                        builder: (context) {
+                          return CupertinoAlertDialog(
+                            title: const Text('예매 하시겠습니까?'),
+                            content: Text('좌석 : $selectedRow-$selectedCol'),
+                            actions: [
+                              CupertinoDialogAction(
+                                child: const Text(
+                                  '취소',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                              CupertinoDialogAction(
+                                child: const Text(
+                                  '확인',
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.purple),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
                     '예매 하기',
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                    ),
-                  ),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.purple),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
                     ),
                   ),
                 ),
@@ -119,60 +170,38 @@ class _SeatPageStatus extends State<StatefulWidget> {
       ),
     );
   }
-}
 
-/// 좌석 열 구분 헤더
-Widget RowsHeader() {
-  List<String> txt = ['A', '', 'B', '', ' ', '', 'C', '', 'D'];
-
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: List.generate(txt.length, (i) {
-      if (txt[i].isNotEmpty) {
-        return Container(
-          alignment: Alignment.bottomCenter,
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-          child: Text('${txt[i]}'),
-        );
-      } else {
-        return SizedBox(width: 4);
-      }
-    }),
-  );
-}
-
-/// 좌석 한 줄
-Widget Rows(int idx) {
-  return Padding(
-    padding: idx != seatNum ? EdgeInsets.only(bottom: 8) : EdgeInsets.zero,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(9, (i) {
-        return (i == 0 || i.isEven)
-            ? seat(row: idx, col: i)
-            : SizedBox(width: 4);
-      }),
-    ),
-  );
-}
-
-/// 좌석 한개
-Widget seat({required int row, required int col}) {
-  return GestureDetector(
-    onTap: () {
-      print('row: $row / col: $col');
-    },
-    child: Container(
-      alignment: Alignment.center,
-      width: 50,
-      height: 50,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: (col == 4) ? null : Colors.grey[300]!,
+  GestureDetector selectSeat(int idx, String seatChar) {
+    int rowNum = idx ~/ 5 + 1;
+    return GestureDetector(
+      onTap: () {
+        if (idx % 5 == 2) return;
+        setState(() {
+          if (selectedSeat != idx) {
+            selectedSeat = idx;
+            selectedCol = seatChar;
+            selectedRow = rowNum;
+          } else {
+            selectedSeat = null;
+            selectedCol = null;
+            selectedRow = null;
+          }
+        });
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: (idx == selectedSeat)
+              ? Colors.purple
+              : (idx % 5 == 2)
+                  ? null
+                  : Colors.grey[300]!,
+        ),
+        child: (idx % 5 == 2) ? Text('$rowNum') : null,
       ),
-      child: (col == 4) ? Text('$row') : null,
-    ),
-  );
+    );
+  }
 }

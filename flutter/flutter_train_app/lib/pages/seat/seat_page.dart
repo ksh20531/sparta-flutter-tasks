a0cf3703e5ef73cc2c1df1220ea1db15c1836417
김서후 <ksh20531@gmail.com>
2024-11-19 14:49:44 +0900

    refactor : Seat Page 위젯 분리

--- flutter/flutter_train_app/lib/pages/seat/seat_page.dart
+++ flutter/flutter_train_app/lib/pages/seat/seat_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const rowCnt = 20;
-const List<String> seatHeader = ['A', 'B', ' ', 'C', 'D'];
+const List<String> seatColHeader = ['A', 'B', ' ', 'C', 'D'];
 
 class SeatPage extends StatefulWidget {
   @override
@@ -22,156 +22,19 @@ class _SeatPageStatus extends State<SeatPage> {
     String? endStation = arguments[1];
 
     return Scaffold(
-      appBar: AppBar(title: Text('좌석 선택')),
+      appBar: AppBar(title: const Text('좌석 선택')),
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      startStation,
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.purple,
-                      ),
-                    ),
-                    Icon(
-                      Icons.arrow_circle_right_outlined,
-                      size: 30,
-                    ),
-                    Text(
-                      endStation,
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              stationInfo(startStation, endStation),
               const SizedBox(height: 8),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 20,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: Colors.purple,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    SizedBox(width: 4),
-                    Text('선택됨'),
-                    SizedBox(width: 20),
-                    Container(
-                      width: 20,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300]!,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    SizedBox(width: 4),
-                    Text('선택안됨'),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(seatHeader.length, (i) {
-                    return Container(
-                      margin: EdgeInsets.only(right: 4),
-                      alignment: Alignment.bottomCenter,
-                      width: 50,
-                      height: 50,
-                      child: Text(seatHeader[i]),
-                    );
-                  }),
-                ),
-              ),
+              seatInfo(),
+              seatHeader(),
               const SizedBox(height: 8),
-              Expanded(
-                child: GridView.builder(
-                    padding: EdgeInsets.symmetric(horizontal: 40),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 5,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8,
-                    ),
-                    itemCount: rowCnt * seatHeader.length,
-                    itemBuilder: (context, idx) {
-                      String seatChar = seatHeader[idx % 5];
-                      return selectSeat(idx, seatChar);
-                    }),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (selectedRow != null && selectedCol != null) {
-                      showCupertinoDialog(
-                        context: context,
-                        builder: (context) {
-                          return CupertinoAlertDialog(
-                            title: const Text('예매 하시겠습니까?'),
-                            content: Text('좌석 : $selectedRow-$selectedCol'),
-                            actions: [
-                              CupertinoDialogAction(
-                                child: const Text(
-                                  '취소',
-                                  style: TextStyle(color: Colors.red),
-                                ),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                              CupertinoDialogAction(
-                                child: const Text(
-                                  '확인',
-                                  style: TextStyle(color: Colors.blue),
-                                ),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(Colors.purple),
-                    shape: WidgetStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  child: const Text(
-                    '예매 하기',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
+              seatGrid(),
+              bookingButton(context),
             ],
           ),
         ),
@@ -179,6 +42,109 @@ class _SeatPageStatus extends State<SeatPage> {
     );
   }
 
+  /// 선택한 출발역 도착역 정보 표시
+  Padding stationInfo(String startStation, String endStation) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            startStation,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple,
+            ),
+          ),
+          Icon(
+            Icons.arrow_circle_right_outlined,
+            size: 30,
+          ),
+          Text(
+            endStation,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// 좌석 구분 정보 표시
+  Padding seatInfo() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 20,
+            height: 20,
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          SizedBox(width: 4),
+          Text('선택됨'),
+          SizedBox(width: 20),
+          Container(
+            width: 20,
+            height: 20,
+            decoration: BoxDecoration(
+              color: Colors.grey[300]!,
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          SizedBox(width: 4),
+          Text('선택안됨'),
+        ],
+      ),
+    );
+  }
+
+  /// 좌석 열 헤더 표시
+  Padding seatHeader() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(seatColHeader.length, (i) {
+          return Container(
+            margin: EdgeInsets.only(right: 4),
+            alignment: Alignment.bottomCenter,
+            width: 50,
+            height: 50,
+            child: Text(seatColHeader[i]),
+          );
+        }),
+      ),
+    );
+  }
+
+  /// 좌석 배치도
+  Expanded seatGrid() {
+    return Expanded(
+      child: GridView.builder(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+          ),
+          itemCount: rowCnt * seatColHeader.length,
+          itemBuilder: (context, idx) {
+            String seatChar = seatColHeader[idx % 5];
+            return selectSeat(idx, seatChar);
+          }),
+    );
+  }
+
+  /// 좌석
   GestureDetector selectSeat(int idx, String seatChar) {
     int rowNum = idx ~/ 5 + 1;
     return GestureDetector(
@@ -209,6 +175,65 @@ class _SeatPageStatus extends State<SeatPage> {
                   : Colors.grey[300]!,
         ),
         child: (idx % 5 == 2) ? Text('$rowNum') : null,
+      ),
+    );
+  }
+
+  /// 예약 하기 버튼
+  SizedBox bookingButton(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          if (selectedRow != null && selectedCol != null) {
+            showCupertinoDialog(
+              context: context,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  title: const Text('예매 하시겠습니까?'),
+                  content: Text('좌석 : $selectedRow-$selectedCol'),
+                  actions: [
+                    CupertinoDialogAction(
+                      child: const Text(
+                        '취소',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    CupertinoDialogAction(
+                      child: const Text(
+                        '확인',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+        },
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(Colors.purple),
+          shape: WidgetStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        child: const Text(
+          '예매 하기',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }

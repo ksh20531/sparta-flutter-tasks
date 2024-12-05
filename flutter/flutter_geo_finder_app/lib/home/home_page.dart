c8354726a8166c38adccd1db2e659b662703f5f3
김서후 <ksh20531@gmail.com>
2024-12-05 16:24:20 +0900

    feat : [HomePage] AppBar TextField 포커스 해제

--- flutter/flutter_geo_finder_app/lib/home/home_page.dart
+++ flutter/flutter_geo_finder_app/lib/home/home_page.dart
@@ -8,17 +8,23 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: TextField(),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.separated(
-              padding: EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 0),
-              itemCount: 3,
-              separatorBuilder: (context, index) => SizedBox(height: 20),
-              itemBuilder: (content, index) => HomePageItem(),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                padding:
+                    EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 0),
+                itemCount: 3,
+                separatorBuilder: (context, index) => SizedBox(height: 20),
+                itemBuilder: (content, index) => HomePageItem(),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

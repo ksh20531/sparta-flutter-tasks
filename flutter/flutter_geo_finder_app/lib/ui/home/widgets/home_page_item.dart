1c1db409e52fff674ca11b91fac96da2e4984e56
김서후 <ksh20531@gmail.com>
2024-12-07 02:22:17 +0900

    Feat : [HomePage] DetailPage와 연결

--- flutter/flutter_geo_finder_app/lib/ui/home/widgets/home_page_item.dart
+++ flutter/flutter_geo_finder_app/lib/ui/home/widgets/home_page_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_geo_finder_app/ui/detail/detail_page.dart';
 
 class HomePageItem extends StatelessWidget {
   const HomePageItem({super.key});
@@ -11,20 +12,27 @@ class HomePageItem extends StatelessWidget {
         border: Border.all(color: Colors.grey[300]!, width: 1.5),
         borderRadius: BorderRadius.circular(16),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('삼성1동 주민센터',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(height: 2),
-            Text('공공,사회기관>행적복지센터',
-                style: TextStyle(fontSize: 16, color: Colors.grey[800])),
-            SizedBox(height: 2),
-            Text('서울특별시 강남구 봉은사로 616 삼성1동 주민센터',
-                style: TextStyle(fontSize: 16, color: Colors.grey[800]))
-          ],
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => DetailPage()));
+        },
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          color: Colors.transparent,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('삼성1동 주민센터',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 2),
+              Text('공공,사회기관>행적복지센터',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[800])),
+              const SizedBox(height: 2),
+              Text('서울특별시 강남구 봉은사로 616 삼성1동 주민센터',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[800]))
+            ],
+          ),
         ),
       ),
     );

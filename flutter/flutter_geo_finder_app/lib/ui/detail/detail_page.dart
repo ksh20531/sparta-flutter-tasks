610f688f22703071439317f0c74b5add504d44e7
김서후 <ksh20531@gmail.com>
2024-12-07 03:48:09 +0900

    Fix : http 페이지 로드 안되는 현상 수정

--- flutter/flutter_geo_finder_app/lib/ui/detail/detail_page.dart
+++ flutter/flutter_geo_finder_app/lib/ui/detail/detail_page.dart
@@ -13,6 +13,9 @@ class DetailPage extends StatelessWidget {
       body: InAppWebView(
         initialUrlRequest: URLRequest(url: WebUri(link)),
         initialSettings: InAppWebViewSettings(
+          javaScriptEnabled: true, // JavaScript 활성화
+          domStorageEnabled: true, // DOM 스토리지 활성화
+          mediaPlaybackRequiresUserGesture: false, // 미디어 재생 허용
           userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537'
               '.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
         ),

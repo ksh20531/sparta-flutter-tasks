8cd5cd0a442af10f6422e1715567858f7476b690
김서후 <ksh20531@gmail.com>
2024-12-26 19:15:39 +0900

    feat : HomePage 인기순 항목 숫자 표시

--- flutter/flutter_movie_info_app/lib/home/widgets/popular.dart
+++ flutter/flutter_movie_info_app/lib/home/widgets/popular.dart
@@ -9,7 +9,7 @@ class Popular extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text(
-          '현재 상영중',
+          '인기순',
           style: TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
@@ -18,20 +18,39 @@ class Popular extends StatelessWidget {
         const SizedBox(height: 16),
         Container(
           height: 180,
-          child: ListView.separated(
+          child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: 20,
             itemBuilder: (BuildContext context, int index) {
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  'assets/images/ToyStory.jpg',
-                  fit: BoxFit.fill,
-                ),
+              return Stack(
+                children: [
+                  Container(
+                    width: 160,
+                    alignment: Alignment.centerRight,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        'assets/images/ToyStory.jpg',
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -20,
+                    left: 14,
+                    child: Text(
+                      '${index + 1}',
+                      style: const TextStyle(
+                        fontSize: 90,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
               );
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(width: 16);
             },
           ),
         ),

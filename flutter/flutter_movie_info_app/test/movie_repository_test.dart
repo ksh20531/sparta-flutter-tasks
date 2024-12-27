21b0bf46c464b1906d99b57d795d28f2b4e5c22e
김서후 <ksh20531@gmail.com>
2024-12-28 04:05:23 +0900

    test : TMDB API 호출 테스트

---
+++ flutter/flutter_movie_info_app/test/movie_repository_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_movie_info_app/data/repository/movie_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  final repo = MovieRepository();
+
+  test('getNowPlaying test', () async {
+    final result = await repo.getNowPlaying();
+    if (result != null) {
+      print(result[0].originalTitle);
+    } else {
+      print('is null');
+    }
+  });
+
+  test('getPopular test', () async {
+    final result = await repo.getPopular();
+    if (result != null) {
+      print(result[0].originalTitle);
+    } else {
+      print('is null');
+    }
+  });
+
+  test('getTopRating test', () async {
+    final result = await repo.getTopRating();
+    if (result != null) {
+      print(result[0].originalTitle);
+    } else {
+      print('is null');
+    }
+  });
+
+  test('getUpcoming test', () async {
+    final result = await repo.getUpcoming();
+    if (result != null) {
+      print(result[0].originalTitle);
+    } else {
+      print('is null');
+    }
+  });
+
+  test('getDetail test', () async {
+    final result = await repo.getDetail(939243);
+    if (result != null) {
+      print(result.originalTitle);
+      expect(result.originalTitle, 'Sonic the Hedgehog 3');
+    } else {
+      print('is null');
+    }
+  });
+}
